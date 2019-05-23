52f2152fa2678448f932a181cbdbd43a6d371348
Johan Höglund <johan.hoglund@preciofishbone.se>
2019-05-23 16:40:31 +0200

    Drag And Drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag
+} from '@angular/cdk/drag-drop';
+import { doesNotThrow } from 'assert';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Drag';
+  shortlist: string[] = [
+    'Daenerys Targaryen',
+    'Cersei Lannister',
+    'Jon Snow',
+    'Arya Stark',
+    'Sandor Clegane',
+    'Ser Bronn James',
+    'Theon Greyjoy',
+    'Brienne of Tarth',
+    'Hodor',
+    'Khal Drogo',
+    'Ned Stark'
+  ];
+
+  favorites: string[] = [
+    'Ramsay Bolton',
+    'Drogon',
+    'Littlefinger'
+];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      if(event.container.data[event.previousIndex] === 'Ramsay Bolton'){
+          alert('Aja baja.');
+      } else {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+        );
+      }
+    } else {
+      console.log('flytta mellan listor');
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+
+  onlyFivePredicate(item: CdkDrag<string[]>): boolean {
+    return item.data.length < 5;
+  }
+
+  noCanDoPredicate(item: CdkDrag<string>): boolean {
+      return item.data !== 'Ramsay Bolton';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {DragDropModule} from '@angular/cdk/drag-drop';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    DragDropModule,
     BrowserModule
   ],
   providers: [],
